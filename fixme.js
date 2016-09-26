1cc133d549c4f33ddcdfd09d961bbd7b558299e6
Natdog18 <rosebailey@Kimberlys-MacBook-Pro-4.local>
2016-09-26 14:25:50 -0400

    init

---
+++ fixme.js
@@ -0,0 +1,105 @@
+// You will use this Object for the answers
+// DO NOT CHANGE THIS SECTION
+var adventuringParty = {
+    wizard: {
+        name: "Gandalf",
+        class: "Wizard",
+        health: 43,
+        mana: 248,
+        weapons: [
+            { name: "Staff", damage: 5 },
+            { name: "Dagger", damage: 1}
+        ]
+    },
+    warrior: {
+        name: "Conan",
+        class: "Warrior",
+        health: 206,
+        mana: 0,
+        weapons: [
+            { name: "Axe", damage: 50 },
+            { name: "Bow", arrow: [
+                { name: "+1 Arrows of Punishing", damage: 40, qty: 3},
+                { name: "Wooden Arrows", damage: 10, qty: 4}
+            ]}
+        ]
+    }
+}
+
+// MAKE CHANGES AFTER THIS
+
+// This function lists all characters in the party
+ aventuringParty.warrior.name = function(){
+function listCharacters(Characters) {
+    console.log("Characters:");
+    for(obj in "party") {
+        console.log(" * " + "party"[obj].name);
+
+}
+
+// This function should list all weapons for the character
+ var listWeapons= function(character) {
+    console.log("Listing weapons for " + character.name);
+    for(var i in character.weapons) {
+        var weapon = character.weapons[i];
+        console.log(" * " . weapon[name]);
+    }
+}
+
+// This function should attack with the specified weapon
+// It uses the weapon damage from the object to determine the maximum damage of this weapon
+function weaponAttack(character, weaponName) {
+    console.log(character.name + " attacks with his " + weaponName);
+    var maxDamage = adventuringParty.worrior.weapons[0].damage;
+    for (var [i]in character.weapons) {
+        var weapon = character.weapons[i];
+        if(weapon.name == weaponName.toUpperCase()) 
+            maxDamage = weapon.damage;
+        }
+    }
+    console.log(adventuringParty.warrior.name + "shoots a wooden arrow from his bow");
+    return Math.round( Math.random() * maxDamage );
+}
+
+// Run the functions
+// 
+    var listWeapons = function(character){
+        console.log("listing weapons for" + character.name);
+        for(var i in character.weapons){
+            var weapon = character.weapons[i];
+            console.log("\t* + weapon.name");
+        }
+    } 
+}
+console.log
+
+
+
+
+
+
+// listCharacters(adventuringParty);
+// listWeapons(adventuringParty.character);
+// listWeapons(adventuringParty.character);
+// weaponAttack("characters", "listWeapons");
+// weaponAttack("Characters", "listweapons");
+
+
+/*
+EXAMPLE OUTPUT:
+
+
+Characters:                                                                                                                                                                                                  
+ * Gandalf                                                                                                                                                                                                   
+ * Conan                                                                                                                                                                                                     
+Listing weapons for Gandalf:                                                                                                                                                                                 
+ * Staff                                                                                                                                                                                                     
+ * Dagger                                                                                                                                                                                                    
+Listing weapons for Conan:                                                                                                                                                                                   
+ * Axe                                                                                                                                                                                                       
+ * Bow                                                                                                                                                                                                       
+Conan attacks with his axe                                                                                                                                                                                   
+He hits for 17 damage                                                                                                                                                                                        
+Gandalf attacks with his STAFF                                                                                                                                                                               
+He hits for 3 damage         
+ */
